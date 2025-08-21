2b2554fab8f88d50fe767315c9b716c2495624bf
Beatriz Lourenco <blourenco@onc.ai>
2025-08-21 16:56:20 -0700

    4.6-wasi and composability

---
+++ chapter04/smart_cms/smartcms_kvstore_guest/guest.js
@@ -0,0 +1,6 @@
+import { get, set } from "component:smartcms/kvstore";
+import storygen from 'storygen';
+export function run() {
+    set("guest-hello", storygen());
+    return get("guest-hello");
+}
---
+++ chapter04/smart_cms/smartcms_ml_guest/src/lib.rs
@@ -0,0 +1,18 @@
+#[allow(warnings)]
+mod bindings;
+
+use bindings::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    /// Say hello!
+    fn storygen() -> String {
+        let stories = std::fs::read_to_string("stories.txt").unwrap();
+        let lines: Vec<&str> = stories.lines().collect();
+        let random = rand::random::<usize>() % lines.len();
+        lines[random].to_string()
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
