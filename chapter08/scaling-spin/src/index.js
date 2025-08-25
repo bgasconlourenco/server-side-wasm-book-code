f0a14c51c4c99866d63b8abfc810556322d67526
Beatriz Lourenco <blourenco@onc.ai>
2025-08-24 20:11:42 -0700

    8.3-Running WASM in kubernetes with wasmcloud

---
+++ chapter08/scaling-spin/src/index.js
@@ -0,0 +1,25 @@
+
+// For AutoRouter documentation refer to https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
+
+let router = AutoRouter();
+
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+    .get('/', () => new Response('Hello, Spin!'))
+    .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+    .get("/load", () => {
+        function fib(n) {
+            if (n < 2) return n;
+            return fib(n - 1) + fib(n - 2);
+        }
+        const result = fib(40); 
+        return new Response(`fib(40) = ${result}`);
+    })
+
+addEventListener('fetch', (event) => {
+    event.respondWith(router.fetch(event.request));
+});
+
